207a93c82084f42d611dc5620fcda9c41a37b690
Marcos Giordano <marcos_l14@hotmail.com>
2025-11-15 10:50:36 -0300

    Refactored routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { PistonKinematics } from './piston-analysis/piston-kinematics/piston-kinematics';
 import { TubingResonance } from './tubing-analysis/tubing-resonance/tubing-resonance';
 
+export const routeNames = {
+  pistonKinematics: 'piston-kinematics',
+  tubingResonance: 'tubing-resonance',
+};
+
 export const routes: Routes = [
   { path: '', component: PistonKinematics },
-  { path: 'piston-kinematics', component: PistonKinematics },
-  { path: 'tubing-resonance', component: TubingResonance },
+  { path: routeNames.pistonKinematics, component: PistonKinematics },
+  { path: routeNames.tubingResonance, component: TubingResonance },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { PistonAnalysisModule } from './piston-analysis/piston-analysis-module';
+import { routeNames } from './app.routes';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,17 @@ import { PistonAnalysisModule } from './piston-analysis/piston-analysis-module';
 })
 export class App {
   protected readonly title = signal('Engine-Calculator');
+  public routes = routeNames;
+  public activeTab = this.routes.pistonKinematics;
 
   constructor(private router: Router) {}
 
   goToPistonKinematics() {
-    this.router.navigate(['/piston-kinematics']);
+    this.router.navigate([`/${this.routes.pistonKinematics}`]);
+    this.activeTab = this.routes.pistonKinematics;
   }
   goToTubingResonance() {
-    this.router.navigate(['/tubing-resonance']);
+    this.router.navigate([`/${this.routes.tubingResonance}`]);
+    this.activeTab = this.routes.tubingResonance;
   }
 }
